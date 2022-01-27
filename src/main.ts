0cac3fe301414474c91a163e17f081952fbe5d27
Andre <aduenas246@gmail.com>
2022-01-27 18:19:54 +0800

    initial queries updated

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { API_VERSION } from './shared/utils/constants.util';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
   app.setGlobalPrefix('tyto');
   const config = new DocumentBuilder()
     .setTitle('Admin Crud')
