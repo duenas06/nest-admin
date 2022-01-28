e106808c405d4c629cbc1649d17e974f3d65e7af
Andre <aduenas246@gmail.com>
2022-01-29 04:14:57 +0800

    teacher and student login with authguard and jwt strategy  added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('TYTO')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
