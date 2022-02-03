ef7d1d593c49f009b445c813cd5f448562e21723
Andre <aduenas246@gmail.com>
2022-02-03 17:24:29 +0800

    validation via id of both accounts added

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import { join } from 'path';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
+declare const module: any;
 
 // import swaggerUi from 'swagger-ui-express';
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('tyto');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -27,9 +26,14 @@ async function bootstrap() {
   });
   SwaggerModule.setup('doc', app, document);
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  // app.useStaticAssets(join(__dirname, '..', 'public'));
+  // app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  // app.setViewEngine('hbs');
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
